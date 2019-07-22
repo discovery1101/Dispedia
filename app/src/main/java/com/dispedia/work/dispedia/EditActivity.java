b48b1e0be6dcb717eba3332fd21faa7559b3dff6
hi-aida <na-ona@ac-tec.co.jp>
2019-07-22 20:48:40 +0900

    會田：[編集画面]編集モードを実装

--- app/src/main/java/com/dispedia/work/dispedia/EditActivity.java
+++ app/src/main/java/com/dispedia/work/dispedia/EditActivity.java
@@ -48,6 +48,10 @@ public class EditActivity extends AppCompatActivity {
         RadioGroup radioEditMode = findViewById(R.id.radioEditMode);
         RadioButton register = findViewById(R.id.registerMode);
 
+        // TODO デバッグ用コード
+        // 現在登録ボタンからしか遷移できず、mode = "R"しか設定されない為、動作確認用にmodeを設定
+        //mode = "E";
+
         // 新規登録の場合
         if ("R".equals(mode)) {
             radioEditMode.check(R.id.registerMode);
@@ -61,22 +65,19 @@ public class EditActivity extends AppCompatActivity {
             EditText inputWordName = findViewById(R.id.inputWordName);
             EditText inputReadName = findViewById(R.id.inputReadName);
             EditText inputContent = findViewById(R.id.inputContent);
-            //TODO 本当は詳細画面から値を持ってきて設定する
-            /*
-            id = intent.getStringExtra("id");
-            initWordName = intent.getStringExtra("word");
-            initReadName = intent.getStringExtra("kana");
-            initContent = intent.getStringExtra("content");
-            */
+            // TODO デバッグ用コード
+            // 本来は詳細画面から値を受け取り設定する
+//            id = intent.getStringExtra("id");
+//            initWordName = intent.getStringExtra("word");
+//            initReadName = intent.getStringExtra("kana");
+//            initContent = intent.getStringExtra("content");
             id = "0005";
             initWordName = "HTML5";
             initReadName = "えいちてぃーえむえるふぁいぶ";
-            initContent = "HyperText Markup Languageの第5版。今まで複雑だった処理が簡単に出来て、HTMLをより構造的にスッキリ書ける。\n\nテキストエリアは、入力が指定したサイズを越えた場合スクロールします。\n\n\n\n\n\n\n\n\n\n\n\nここまで";
+            initContent = "HyperText Markup Languageの第5版。";
             inputWordName.setText(initWordName);
             inputReadName.setText(initReadName);
             inputContent.setText(initContent);
-
-            // TODO エラーメッセージがある場合、メッセージエリアにメッセージを表示
         }
 
         radioEditMode.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
@@ -131,7 +132,6 @@ public class EditActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-
                 // SQLite接続のための初期処理
                 if(helper == null){
                     helper = new SqliteOpenHelper(getApplicationContext());
@@ -147,59 +147,30 @@ public class EditActivity extends AppCompatActivity {
                 RadioButton edit = findViewById(R.id.editMode);
                 RadioButton delete = findViewById(R.id.deleteMode);
 
+                // 項目を変数に格納
+                EditText inputWordName = findViewById(R.id.inputWordName);
+                EditText inputReadName = findViewById(R.id.inputReadName);
+                EditText inputContent = findViewById(R.id.inputContent);
+                word = inputWordName.getText().toString();
+                kana = inputReadName.getText().toString();
+                content = inputContent.getText().toString();
+
                 // 登録の場合
                 if (register.isChecked()) {
-                    EditText inputWordName = findViewById(R.id.inputWordName);
-                    EditText inputReadName = findViewById(R.id.inputReadName);
-                    EditText inputContent = findViewById(R.id.inputContent);
-
-                    word = inputWordName.getText().toString();
-                    kana = inputReadName.getText().toString();
-                    content = inputContent.getText().toString();
-                    boolean isError = false;
-                    ArrayList<String> messages = new ArrayList<String>();
-                    String errMsg = "";
-
-                    // TODO 入力チェック
-                    if(word.isEmpty()) {
-                        messages.add("単語名");
-                        isError = true;
-                    }
-                    if(kana.isEmpty()) {
-                        messages.add("よみ");
-                        isError = true;
-                    }
-                    if(content.isEmpty()) {
-                        messages.add("意味");
-                        isError = true;
-                    }
-
-                    if(isError) {
-                        //Intent intent = new Intent(getApplication(), EditActivity.class);
-                        //intent.putExtra("errorMsg", msg);
-                        //startActivity(intent);
-                        // エラーメッセージを格納
-                        for(int i = 0; i < messages.size(); i++) {
-                            errMsg = errMsg + messages.get(i) + '、';
-                            if(i == messages.size() - 1) {
-                                errMsg = errMsg.substring(0, errMsg.length() - 1) + "が未入力です。";
-                            }
-                        }
-                        setMsg(msg, errMsg, Color.RED);
-
-                    } else {
+                    boolean isError = isNullError(word, kana, content);
+                    if (!isError) {
                         long insertData = insertData(db, id, word, kana, content);
 
-                        // TODO DB接続をcloseする
-
-                        // TODO エラーハンドリング
                         // 正常の場合、詳細画面に遷移し登録完了のメッセージを表示する。
                         if (insertData != -1) {
                             Intent intent = new Intent(getApplication(), DetailActivity.class);
+
+                            intent.putExtra("id", id);
                             intent.putExtra("word", word);
                             intent.putExtra("kana", kana);
                             intent.putExtra("content", content);
                             intent.putExtra("mode", "R");
+
                             startActivity(intent);
 
                             // DBの通信処理が不正の場合、エラーメッセージを表示する。
@@ -210,7 +181,30 @@ public class EditActivity extends AppCompatActivity {
 
                     // 編集の場合
                 } else if(edit.isChecked()) {
-                    // TODO
+                    boolean isError = isNullError(word, kana, content);
+                    if (!isError) {
+
+                        int updateData = updateData(db, id, word, kana, content);
+
+                        // 正常の場合、詳細画面に遷移し登録完了のメッセージを表示する。
+                        // SQLiteDatabase.insert の戻り値は更新した行数のため、1行の場合正常
+                        if (updateData == 1) {
+                            Intent intent = new Intent(getApplication(), DetailActivity.class);
+                            // TODO 登録時にidを生成していない
+                            // TODO idを詳細画面に引き渡せていない
+                            intent.putExtra("id", id);
+                            intent.putExtra("word", word);
+                            intent.putExtra("kana", kana);
+                            intent.putExtra("content", content);
+                            intent.putExtra("mode", "E");
+
+                            startActivity(intent);
+
+                            // DBの通信処理が不正の場合、エラーメッセージを表示する。
+                        } else {
+                            setMsg(msg, "登録に失敗しました。", Color.RED);
+                        }
+                    }
 
                     // 削除の場合
                 } else if(delete.isChecked()) {
@@ -222,11 +216,10 @@ public class EditActivity extends AppCompatActivity {
         });
     }
 
-    // TODO
-    private long insertData(SQLiteDatabase db, String id, String word, String kana,String content) {
+    private int updateData(SQLiteDatabase db,String id, String word, String kana, String content) {
 
         ContentValues values = new ContentValues();
-        values.put("ITEM_ID", id);
+        // values.put("ITEM_ID", id);
         values.put("ITEM_NAME", word);
         values.put("ITEM_KANA", kana);
         // values.put("ITEM_INDEX", int 0); // index + 1 取得する
@@ -241,7 +234,67 @@ public class EditActivity extends AppCompatActivity {
         values.put("REGIST_DATE", "");
         values.put("UPDATE_DATE", "");
 
+        String whereClause = "ITEM_ID = ?";
+        String[] whereArgs = new String[]{id};
+
+        return db.update("DP_DICTIONARY", values, whereClause, whereArgs);
+}
+
+    private long insertData(SQLiteDatabase db,String id, String word, String kana, String content) {
+
+        ContentValues values = new ContentValues();
+        values.put("ITEM_ID", id);
+        values.put("ITEM_NAME", word);
+        values.put("ITEM_KANA", kana);
+        // values.put("ITEM_INDEX", int 0); // index + 1 を取得する必要あり？
+        values.put("CONTENT", content);
+        values.put("CATEGORY", "");
+        values.put("TAG1", "");
+        values.put("TAG2", "");
+        values.put("TAG3", "");
+        values.put("TAG4", "");
+        values.put("TAG5", "");
+        values.put("DELETE_FLG", "");
+        values.put("REGIST_DATE", "");
+        values.put("UPDATE_DATE", "");
+
         return db.insert("DP_DICTIONARY", null, values);
+    }
+
+    private boolean isNullError(String word, String kana, String content) {
+        boolean isError = false;
+        ArrayList<String> messages = new ArrayList<String>();
+        TextView msg = findViewById(R.id.errorMsg);
+        String errMsg = "";
+
+        if(word.isEmpty()) {
+            messages.add("単語名");
+            isError = true;
+        }
+        if(kana.isEmpty()) {
+            messages.add("よみ");
+            isError = true;
+        }
+        if(content.isEmpty()) {
+            messages.add("意味");
+            isError = true;
+        }
+
+        if(isError) {
+            //Intent intent = new Intent(getApplication(), EditActivity.class);
+            //intent.putExtra("errorMsg", msg);
+            //startActivity(intent);
+            // エラーメッセージを格納
+            for (int i = 0; i < messages.size(); i++) {
+                errMsg = errMsg + messages.get(i) + '、';
+                if (i == messages.size() - 1) {
+                    errMsg = errMsg.substring(0, errMsg.length() - 1) + "が未入力です。";
+                }
+            }
+            setMsg(msg, errMsg, Color.RED);
+        }
+
+        return isError;
     }
 
     private void setMsg(TextView msg, String msgText, int msgColor) {
